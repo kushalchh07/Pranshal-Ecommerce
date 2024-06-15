7f7dd5560d47dd4c9bf22856c9bdebab7874ca94
kushalchh07 <kushalchhetri54@gmail.com>
2024-06-15 18:29:37 +0545

    feat: added required utilies

--- lib/consts/colors.dart
+++ lib/consts/colors.dart
@@ -7,3 +7,6 @@ const Color whiteColor = Color.fromRGBO(255, 255, 255, 1);
 const Color lightGrey = Color.fromRGBO(239, 239, 239, 1);
 const Color redColor = Color.fromRGBO(230, 46, 4, 1);
 const Color golden = Color.fromRGBO(255, 168, 0, 1);
+
+const Color primaryColor = Color.fromARGB(255, 117, 63, 0);
+const Color secondaryColor = Color.fromARGB(255, 255, 145, 1);
--- lib/consts/strings.dart
+++ lib/consts/strings.dart
@@ -1,3 +1,3 @@
-const appname = "SastoSaman";
+const appname = "NepStyle";
 const appversion = "Version 1.0.0";
 const credits = "@kush";
