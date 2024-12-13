39d44d3f086e4b27fc5e637157eb9772ad8ee4ea
kushalchh07 <kushalchhetri54@gmail.com>
2024-12-13 21:17:59 +0545

    feat: error removed to run on android device

--- lib/core/utils/network_utils.dart
+++ lib/core/utils/network_utils.dart
@@ -1,8 +1,8 @@
-import 'package:connectivity/connectivity.dart';
+// import 'package:connectivity/connectivity.dart';
 
-class NetworkUtils {
-  static Future<bool> isConnected() async {
-    var connectivityResult = await (Connectivity().checkConnectivity());
-    return connectivityResult != ConnectivityResult.none;
-  }
-}
+// class NetworkUtils {
+//   static Future<bool> isConnected() async {
+//     var connectivityResult = await (Connectivity().checkConnectivity());
+//     return connectivityResult != ConnectivityResult.none;
+//   }
+// }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import connectivity_macos
 import geolocator_apple
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
