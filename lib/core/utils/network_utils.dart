ae090ba54efbb5d0097efeabb49fe4853ecd1399
kushalchh07 <kushalchhetri54@gmail.com>
2024-12-18 22:29:24 +0545

    feat: network utils added

--- lib/core/utils/network_utils.dart
+++
@@ -1,8 +0,0 @@
-// import 'package:connectivity/connectivity.dart';
-
-// class NetworkUtils {
-//   static Future<bool> isConnected() async {
-//     var connectivityResult = await (Connectivity().checkConnectivity());
-//     return connectivityResult != ConnectivityResult.none;
-//   }
-// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:pranshal_ecommerce/core/theme/ThemeCubit/theme_cubit.dart';
 
 import 'core/navigation/routes/app_router.dart';
+import 'core/theme/app_theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,11 +13,23 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Pranshal Clothing',
-      initialRoute: AppRouter.splash,
-      onGenerateRoute: AppRouter.generateRoute,
-      debugShowCheckedModeBanner: false,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => ThemeCubit(),
+        ),
+      ],
+      child: BlocBuilder<ThemeCubit, ThemeState>(
+        builder: (context, state) {
+          return GetMaterialApp(
+            theme: AppTheme.themeData(false, context),
+            title: 'Pranshal Clothing',
+            initialRoute: AppRouter.splash,
+            onGenerateRoute: AppRouter.generateRoute,
+            debugShowCheckedModeBanner: false,
+          );
+        },
+      ),
     );
   }
 }
